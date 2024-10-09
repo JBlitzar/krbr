c65d278c8ab7fe30b610051136ae823035b2f8e3
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-10-09 13:42:19 -0700

    Update emb.js

--- emb.js
+++ emb.js
@@ -1,22 +1,23 @@
 setTimeout(() => {
+    var h = btoa(window.location.href)
     for(var i=0;i<100;i++){
       window.history.pushState(
       null,
       document.title,
-      "/krbr/f?f="+btoa(window.location.href)
+      "/krbr/f?f="+h
     );
     }
     window.history.replaceState(
       null,
       document.title,
-      "/krbr/f?f="+btoa(window.location.href)
+      "/krbr/f?f="+h
     );
     
     
     const inIframe = () => window.self !== window.top;
     if (inIframe() || window.location.hostname != "jblitzar.github.io") {
       window.location =
-        "https://jblitzar.github.io/krbr/f?f="+btoa(window.location.href);
+        "https://jblitzar.github.io/krbr/f?f="+h;
     }
   }, 1000);
 
@@ -34,4 +35,4 @@ setTimeout(()=>{window.location = "https://classroom.google.com"},5*60*60*1000)
 window.onblur = function () {
     window.location = "https://classroom.google.com";
 };
-sessionStorage.setItem("_krbr", btoa(window.location.href))
+sessionStorage.setItem("_krbr", "1")
