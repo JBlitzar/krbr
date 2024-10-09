3eaced47c99aa15d053854b3e08ccd17367f1d42
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-10-09 12:26:12 -0700

    yesir

---
+++ c/client.js
@@ -0,0 +1,9 @@
+setTimeout(()=>{
+    if(sessionStorage.getItem("_krbr") != btoa(window.location.href)){
+        window.location.href = "/krbr/f?f="+btoa(window.location.href);
+    }
+},1000);
+
+
+
+
